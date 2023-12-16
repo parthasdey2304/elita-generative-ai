611a244b1cfaa3d7a3b574fda386b67396158ede
Partha <parthasdeyofficial@gmail.com>
2023-12-16 14:03:39 +0530

    initial commit

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ sudo cp elita.py /bin
 
 sudo chmod +x /bin/elita
 sudo chmod +x /bin/elita.py
+
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install python-is-python3 -y
+pip install requests
+pip install pygments
