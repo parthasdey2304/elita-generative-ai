240e15c980da69f711421fae08a5d1620508a585
Partha <parthasdeyofficial@gmail.com>
2023-12-18 12:26:12 +0530

    initial commit

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+echo -e "\e32m[INFO] Getting things ready for you!"
 
+sleep 1
+
+echo "[INFO] Setting up the scripts..."
 sudo cp elita /bin
 sudo cp elita.py /bin
-
 sudo chmod +x /bin/elita
 sudo chmod +x /bin/elita.py
 
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-sudo apt install python-is-python3 -y
+echo "[INFO] Installing Dependencies..."
 pip install requests
 pip install pygments
+
+echo -e "Now you are good to go! Type \e[31m elita 'good morning'\e[32m and see Elita generate content for you!"
