865fe4c39752aa1cd23e9663086beb815b40344e
Partha <parthasdeyofficial@gmail.com>
2023-12-19 15:16:25 +0530

    initial commit

--- remove.sh
+++ remove.sh
@@ -1,2 +1,5 @@
+echo -e "\e[31m[INFO] Removing the Scripts...\e[0m"
 sudo rm -rf /bin/elita
 sudo rm -rf /bin/elita.py
+
+echo -e "\e[36mThanks for using ELITA! Make sure you have starred our GitHub repo!! \e[34mhttps://github.com/parthasdey2304/elita-generative-ai.git \e[0m."
